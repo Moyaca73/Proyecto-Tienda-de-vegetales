40aa1ff7b082711d2ad0c7a99785580318fa1096
AntonioMoya <antonio.moyacalvo@iesjuandeherrera.net>
2021-10-08 17:06:03 +0200

    Cambios cambios cambios

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,2 @@
 let nombre = prompt("¿Cómo te llamas?");
 document.write(`<h2>Esta es la tienda de vegetales de ${nombre}</h2>`);
-document.write(`<p>¿Estás seguro que quieres montar una tienda de vegetales? ${nombre}</p>`);
